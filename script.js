c1766be6ae8399ac380bbaac5aee032b196772ac
Mohankrishna1012 <144467561+Mohankrishna1012@users.noreply.github.com>
2023-10-16 20:51:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+/////// CALL BACK HELL ////////
+
+let div = document.createElement("div");
+div.style.display='flex';
+div.style.justifyContent="center"
+div.style.marginTop="250px"
+div.style.fontSize="500%"
+let second = 10;
+div.innerHTML=second;
+document.body.append(div)
+setTimeout(() => {
+    second -= 1
+    div.innerHTML=second
+    setTimeout(() => {
+        second -= 1
+        div.innerHTML=second
+        setTimeout(() => {
+            second -= 1
+            div.innerHTML=second
+            setTimeout(() => {
+                second -= 1
+                div.innerHTML=second
+                setTimeout(() => {
+                    second -= 1
+                    div.innerHTML=second
+                    setTimeout(() => {
+                        second -= 1
+                        div.innerHTML=second
+                        setTimeout(() => {
+                            second -= 1
+                            div.innerHTML=second
+                            setTimeout(() => {
+                                second -= 1
+                                div.innerHTML=second
+                                setTimeout(() => {
+                                    second -= 1
+                                    div.innerHTML=second
+                                    setTimeout(() => {
+                                        second -= 1
+                                        div.innerHTML="Happy Independance Day"
+                                        
+                                    }, 1000);
+                                }, 1000); 
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);  
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+
+
+
